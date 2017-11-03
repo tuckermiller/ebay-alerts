d10589d40f64e18b9e06d9695d65d368a0875c6f
tuckermiller <tucker.miller@gmail.com>
2017-11-03 12:20:37 -0700

    removed static

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ pool.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
