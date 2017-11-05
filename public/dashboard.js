059e61707696257995d711414d1bcd283ccdd7dc
tuckermiller <tucker.miller@gmail.com>
2017-11-04 20:10:59 -0700

    Removed type expectation for ajax req

--- public/dashboard.js
+++ public/dashboard.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $.ajax(({
         type: "GET",
         url: "/alerts",
-        data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function(data) {
